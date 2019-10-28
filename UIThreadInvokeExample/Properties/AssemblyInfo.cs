421f2c68c7bb266df99fa3b98f8b6ed383274822
SeppPenner <HansM2013@gmx.de>
2019-10-28 19:49:53 +0100

    Updated nuget packages, added GitVersionTask.

--- UIThreadInvokeExample/Properties/AssemblyInfo.cs
+++ UIThreadInvokeExample/Properties/AssemblyInfo.cs
@@ -23,17 +23,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("4a236434-4fbb-4ffc-8d58-732a97cfe7c1")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
